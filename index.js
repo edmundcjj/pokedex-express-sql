611e15d968d00babb0a19443c28b15b25f4096c9
Edmund <edmundcjj@gmail.com>
2018-03-28 01:30:08 +0800

    Solved the client.end() error

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const exphbs = require('express-handlebars');
-const pg = require('pg');
+const { Client } = require('pg');
 
 // set all of the configuration in an object
-const configs = {
-  user: 'postgres',           // Name of the user that is linked to the database
-  host: '127.0.0.1',          // Where the database is hosted
-  database: 'pokemons',         // Name of the database
-  port: 5432,                 // Port number database is connected to
-  password: 'edmund92chow'    // Password to connect to the database
-};
-
-// create a new instance of the client
-const client = new pg.Client(configs);
-
-client.connect((err) => {
-  if (err) console.error('connection error:', err.stack);
-});
+// const configs = {
+//   user: 'postgres',           // Name of the user that is linked to the database
+//   host: '127.0.0.1',          // Where the database is hosted
+//   database: 'pokemons',         // Name of the database
+//   port: 5432,                 // Port number database is connected to
+//   password: 'edmund92chow'    // Password to connect to the database
+// };
+//
+// // create a new instance of the client
+// const client = new pg.Client(configs);
+//
+// client.connect((err) => {
+//   if (err) console.error('connection error:', err.stack);
+// });
 
 /**
  * ===================================
@@ -49,26 +49,39 @@ app.set('view engine', 'handlebars');
 // GET / should return HTML page showing all pokemons currently in database
 // (specifically in the pokemon table within the database)
 app.get('/', (req, response) => {
-  let context = {
-    pokemon: []
-  };
-  // query database for all pokemon
-  const queryString = 'SELECT name FROM pokemon';
+  // Initialise postgres client
+  const client = new Client ({
+    user: 'postgres',           // Name of the user that is linked to the database
+    host: '127.0.0.1',          // Where the database is hosted
+    database: 'pokemons',         // Name of the database
+    port: 5432,                 // Port number database is connected to
+    password: 'edmund92chow'    // Password to connect to the database
+  });
 
-  client.query(queryString, (err, res2) => {
-    if (err) {
-      console.error('query error => ', err.stack);
-    } else {
-      context.pokemon = res2.rows;
-      console.log(context);
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
 
-      // respond with HTML page displaying all pokemon
-      console.log("Before rendering...");
-      response.render('home', context);
-      console.log("After rendering...");
-    }
-    // Close the client connection
-    // client.end();
+    let context = {
+      pokemon: []
+    };
+    // query database for all pokemon
+    const queryString = 'SELECT name FROM pokemon ORDER BY id';
+
+    client.query(queryString, (err, res2) => {
+      if (err) {
+        console.error('query error => ', err.stack);
+      } else {
+        context.pokemon = res2.rows;
+        console.log(context);
+
+        // respond with HTML page displaying all pokemon
+        console.log("Before rendering...");
+        response.render('home', context);
+        console.log("After rendering...");
+      }
+      // Close the client connection
+      client.end();
+    });
   });
 });
 
@@ -84,22 +97,36 @@ app.get('/new', (request, response) => {
 // POST / should create a new pokemon and insert a new entry in the pokemon table,
 // and should redirect to the home page /
 app.post('/', (req, response) => {
+  // Initialise postgres client
+  const client = new Client ({
+    user: 'postgres',           // Name of the user that is linked to the database
+    host: '127.0.0.1',          // Where the database is hosted
+    database: 'pokemons',         // Name of the database
+    port: 5432,                 // Port number database is connected to
+    password: 'edmund92chow'    // Password to connect to the database
+  });
+
   let pokemon = req.body;
 
   const queryString = 'INSERT INTO pokemon(id, num, name, img, weight, height) VALUES($1, $2, $3, $4, $5, $6)'
   const values = [pokemon.id, pokemon.num, pokemon.name, pokemon.img, pokemon.weight, pokemon.height];
 
-  client.query(queryString, values, (err, res) => {
-    if (err) {
-      console.error('query error:', err.stack);
-    } else {
-      console.log('query result:', res);
 
-      // redirect to home page
-      response.redirect('/');
-    }
-    // Close the client connection
-    // client.end();
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
+
+    client.query(queryString, values, (err, res) => {
+      if (err) {
+        console.error('query error:', err.stack);
+      } else {
+        console.log('query result:', res);
+
+        // redirect to home page
+        response.redirect('/');
+      }
+      // Close the client connection
+      client.end();
+    });
   });
 });
 
@@ -107,35 +134,48 @@ app.post('/', (req, response) => {
 // GET /:id (eg. /2) should return HTML page showing information about pokemon
 // with primary ID 2 (read: primary ID, not num property)
 app.get('/:id', (request, response) => {
+  // Initialise postgres client
+  const client = new Client ({
+    user: 'postgres',           // Name of the user that is linked to the database
+    host: '127.0.0.1',          // Where the database is hosted
+    database: 'pokemons',         // Name of the database
+    port: 5432,                 // Port number database is connected to
+    password: 'edmund92chow'    // Password to connect to the database
+  });
+
   let poke_id = request.params.id;
 
   // query database for pokemon with the specified id mentioned in the url
   const queryString = 'SELECT * FROM pokemon WHERE pokemon.id = ' + poke_id;
 
-  client.query(queryString, (err, res2) => {
-    if (err) {
-      console.error('query error => ', err.stack);
-    } else {
-      console.log(res2.rows[0]);
-      let context = {
-        pokemon: {
-          id: res2.rows[0].id,
-          num: res2.rows[0].num,
-          name: res2.rows[0].name,
-          img: res2.rows[0].img,
-          weight: res2.rows[0].weight,
-          height: res2.rows[0].height
-        }
-      }
-      console.log(context);
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
 
-      // respond with HTML page displaying all pokemon
-      console.log("Before rendering...");
-      response.render('pokemon', context);
-      console.log("After rendering...");
-    }
-    // Close the client connection
-    // client.end();
+    client.query(queryString, (err, res2) => {
+      if (err) {
+        console.error('query error => ', err.stack);
+      } else {
+        console.log(res2.rows[0]);
+        let context = {
+          pokemon: {
+            id: res2.rows[0].id,
+            num: res2.rows[0].num,
+            name: res2.rows[0].name,
+            img: res2.rows[0].img,
+            weight: res2.rows[0].weight,
+            height: res2.rows[0].height
+          }
+        }
+        console.log(context);
+
+        // respond with HTML page displaying all pokemon
+        console.log("Before rendering...");
+        response.render('pokemon', context);
+        console.log("After rendering...");
+      }
+      // Close the client connection
+      client.end();
+    });
   });
 });
 
@@ -144,35 +184,48 @@ app.get('/:id', (request, response) => {
 // pre-populated with that pokemon's data - upon submit,
 // it should send PUT request to /:id
 app.get('/:id/edit', (request, response) => {
+  // Initialise postgres client
+  const client = new Client ({
+    user: 'postgres',           // Name of the user that is linked to the database
+    host: '127.0.0.1',          // Where the database is hosted
+    database: 'pokemons',         // Name of the database
+    port: 5432,                 // Port number database is connected to
+    password: 'edmund92chow'    // Password to connect to the database
+  });
+
   let poke_id = request.params.id;
 
   // query database for pokemon with the specified id mentioned in the url
   const queryString = 'SELECT * FROM pokemon WHERE pokemon.id = ' + poke_id;
 
-  client.query(queryString, (err, res2) => {
-    if (err) {
-      console.error('query error => ', err.stack);
-    } else {
-      console.log(res2.rows[0]);
-      let context = {
-        pokemon: {
-          id: res2.rows[0].id,
-          num: res2.rows[0].num,
-          name: res2.rows[0].name,
-          img: res2.rows[0].img,
-          weight: res2.rows[0].weight,
-          height: res2.rows[0].height
-        }
-      }
-      console.log(context);
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
 
-      // respond with HTML page displaying all pokemon
-      console.log("Before rendering...");
-      response.render('edit', context);
-      console.log("After rendering...");
-    }
-    // Close the client connection
-    // client.end();
+    client.query(queryString, (err, res2) => {
+      if (err) {
+        console.error('query error => ', err.stack);
+      } else {
+        console.log(res2.rows[0]);
+        let context = {
+          pokemon: {
+            id: res2.rows[0].id,
+            num: res2.rows[0].num,
+            name: res2.rows[0].name,
+            img: res2.rows[0].img,
+            weight: res2.rows[0].weight,
+            height: res2.rows[0].height
+          }
+        }
+        console.log(context);
+
+        // respond with HTML page displaying all pokemon
+        console.log("Before rendering...");
+        response.render('edit', context);
+        console.log("After rendering...");
+      }
+      // Close the client connection
+      client.end();
+    });
   });
 });
 
@@ -180,24 +233,37 @@ app.get('/:id/edit', (request, response) => {
 // PUT /:id should update the data of the pokemon with the specified ID,
 // and should redirect to the pokemon detail page /:id
 app.put('/:id', (request, response) => {
+  // Initialise postgres client
+  const client = new Client ({
+    user: 'postgres',           // Name of the user that is linked to the database
+    host: '127.0.0.1',          // Where the database is hosted
+    database: 'pokemons',         // Name of the database
+    port: 5432,                 // Port number database is connected to
+    password: 'edmund92chow'    // Password to connect to the database
+  });
+
   let poke_id = request.params.id;
 
   // query database for pokemon with the specified id mentioned in the url
   const queryString = 'UPDATE pokemon SET name=$1, img=$2, height=$3, weight=$4 WHERE pokemon.id = $5';
   const values = [request.body.name, request.body.img, request.body.height, request.body.weight, parseInt(poke_id)];
 
-  client.query(queryString, values, (err, res2) => {
-    if (err) {
-      console.error('query error => ', err.stack);
-    } else {
-      console.log(res2);
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
 
-      console.log("Before rendering...");
-      response.redirect('/' + poke_id);
-      console.log("After rendering...");
-    }
-    // Close the client connection
-    // client.end();
+    client.query(queryString, values, (err, res2) => {
+      if (err) {
+        console.error('query error => ', err.stack);
+      } else {
+        console.log(res2);
+
+        console.log("Before rendering...");
+        response.redirect('/' + poke_id);
+        console.log("After rendering...");
+      }
+      // Close the client connection
+      client.end();
+    });
   });
 });
 
@@ -205,24 +271,37 @@ app.put('/:id', (request, response) => {
 // DELETE /:id should delete the entry of the pokemon with the specified ID,
 // and should redirect to the home page /
 app.delete('/:id', (request, response) => {
+  // Initialise postgres client
+  const client = new Client ({
+    user: 'postgres',           // Name of the user that is linked to the database
+    host: '127.0.0.1',          // Where the database is hosted
+    database: 'pokemons',         // Name of the database
+    port: 5432,                 // Port number database is connected to
+    password: 'edmund92chow'    // Password to connect to the database
+  });
+
   let poke_id = request.params.id;
 
   // query database for pokemon with the specified id mentioned in the url
   const queryString = 'DELETE FROM pokemon WHERE pokemon.id = $1';
   const values = [poke_id];
 
-  client.query(queryString, values, (err, res2) => {
-    if (err) {
-      console.error('query error => ', err.stack);
-    } else {
-      console.log(res2);
+  client.connect((err) => {
+    if (err) console.error('connection error:', err.stack);
 
-      console.log("Before rendering...");
-      response.redirect('/');
-      console.log("After rendering...");
-    }
-    // Close the client connection
-    // client.end();
+    client.query(queryString, values, (err, res2) => {
+      if (err) {
+        console.error('query error => ', err.stack);
+      } else {
+        console.log(res2);
+
+        console.log("Before rendering...");
+        response.redirect('/');
+        console.log("After rendering...");
+      }
+      // Close the client connection
+      client.end();
+    });
   });
 });
 
